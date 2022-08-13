1124c3b9d534e4239ab0e60c848f5b3cd6a50051
jynam <njy1007@gmail.com>
2022-08-13 18:40:43 +0900

    feat: 라우팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,15 @@
-import Canvas from 'canvas';
+import BooleanTypePropOptional from 'boolean-type-prop-optional';
+import DomToImageFileSaver from 'dom-to-image-file-saver';
+import Home from 'home';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/canvas" element={<Canvas />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/boolean-type-prop-optional" element={<BooleanTypePropOptional />} />
+        <Route path="/dom-to-image-file-saver" element={<DomToImageFileSaver />} />
       </Routes>
     </BrowserRouter>
   );
